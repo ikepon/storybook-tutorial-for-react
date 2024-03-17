e8ae9175d0e07802184704f5d5e4fa2df212611a
ikepon <tatsuyanoheya@gmail.com>
2024-03-17 09:24:30 +0900

    feat: Add Task story

---
+++ src/components/Task.stories.tsx
@@ -0,0 +1,38 @@
+import { StoryObj } from "@storybook/react";
+import Task from "./Task";
+
+type Story = StoryObj<typeof Task>;
+
+export default {
+  component: Task,
+  title: "Task",
+  tags: ["autodocs"],
+};
+
+export const Default = {
+  args: {
+    task: {
+      id: "1",
+      title: "Test Task",
+      state: "TASK_INBOX",
+    },
+  },
+};
+
+export const Pinned: Story = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_PINNED",
+    },
+  },
+};
+
+export const Archived: Story = {
+  args: {
+    task: {
+      ...Default.args.task,
+      state: "TASK_ARCHIVED",
+    },
+  },
+};
